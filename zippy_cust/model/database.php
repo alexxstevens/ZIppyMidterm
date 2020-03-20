24028f5f3a88e266f9bc981deb8a1c1a823bd19b
alexxstevens <41077253+alexxstevens@users.noreply.github.com>
2020-03-19 22:02:23 -0500

    password fix

--- zippy_cust/model/database.php
+++ zippy_cust/model/database.php
@@ -1,7 +1,7 @@
 <?php
     $dsn = 'mysql:host=kil9uzd3tgem3naa.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=p0cky0p5l6tghder';
     $username = 'heccedejen5tm9p9';
-    $password = 's06uh7v07t8s4uxm!';
+    $password = 's06uh7v07t8s4uxm';
 
     try {
         $db = new PDO($dsn, $username, $password);
