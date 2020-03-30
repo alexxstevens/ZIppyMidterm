1531b7cc873b7d70d8d9740b3549fc7d02365144
alexxstevens <41077253+alexxstevens@users.noreply.github.com>
2020-03-30 15:04:59 -0500

    fix default sort by price code

--- zippy_cust/view/header.php
+++ zippy_cust/view/header.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <!-- the head section -->
 <head>
     <title>Zippy's Admin Page</title>
     <link rel="icon" href="../../images/logo.png" type="image/icon">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Monoton|Orbitron:700&display=swap" rel="stylesheet">
