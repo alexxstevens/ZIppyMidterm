a7cfbef34b726e080baa8c7e34189812d3f89233
alexxstevens <41077253+alexxstevens@users.noreply.github.com>
2020-04-01 18:17:24 -0500

    added appropriate logout response

--- zippy_cust/view/logout.php
+++ zippy_cust/view/logout.php
@@ -1,6 +1,25 @@
 
 <?php 
-include 'header.php';
+include 'header.php';?>
+
+<br>
+<p class="center-text"><?php if (isset($_SESSION['firstName'])) {?>Thank you for signing out, <?php echo $_SESSION['firstName'];?>!  <?php }?><a href="../controller/index.php">Click Here</a> to view our vehicle inventory.</p>
+
+<?php 
+
+//delete session variable
+unset ($_SESSION['firstName']);
+//destroy session
 session_destroy();
-header("Location: ../controller/index.php");
+//delete session cookie
+$name = session_name();
+$expire = strtotime('-1 year');
+$params = session_get_cookie_params();
+$path = $params['path'];
+$domain = $params['domain'];
+$secure = $params['secure'];
+$httponly = $params['httponly'];
+setcookie ($name, '', $expire, $path, $domain, $secure, $httponly);
+
+include 'footer.php';
 ?>
